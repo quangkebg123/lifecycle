4b5ef8d5ea606e2b01f64a9f8fd97067e967bafc
quang.nguyenmanh <quang.nguyenmanh@vndirect.com.vn>
2019-11-29 13:46:31 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Message } from "./Message";
+import { List } from "./List";
+import { ExternalCounter } from './ExternalCounter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        }
+    }
+
+    incrementCounter = () => {
+        this.setState({ counter: this.state.counter + 1 });
+    }
+
+    render() {
+        console.log("Render App Component");
+        return <div className="container text-center">
+                    <div className="row p-2">
+                        <div className="col-4">
+                            <Message message={`Counter: ${this.state.counter}`}
+                                callback={this.incrementCounter}
+                                text="Increment Counter" />
+                        </div>
+                        <div className="col-4">
+                            <List />
+                        </div>
+                        <div className="col-4">
+                            <ExternalCounter />
+                        </div>
+                    </div>
+                </div>
+    }
 }
-
-export default App;
